ab21876320d19c4220684804b15753859d68e897
Madina Karachai <madina.karachai@ru.axxonsoft.com>
2019-06-19 16:41:31 +0300

    Added htmlWebpackPlugin and cleanWebpackPlugin plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -11,13 +11,13 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-    // plugins: [
-    //     // new CleanWebpackPlugin(),
-    //     new HtmlWebpackPlugin({
-    //         title: 'Madina Karachai - Columns',
-    //         //link: 'rel="shortcut icon" href="../tree.ico" type="image/x-icon"'
-    //     })
-    // ],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Madina Karachai - Columns',
+            //link: 'rel="shortcut icon" href="../tree.ico" type="image/x-icon"'
+        })
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
