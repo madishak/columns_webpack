321faffdaf140a3a1ec25c10bc4755a35aac7b43
Madina Karachai <madina.karachai@ru.axxonsoft.com>
2019-07-12 17:05:46 +0300

    Modified page.js file

--- src/page.js
+++ src/page.js
@@ -1,53 +1,26 @@
 import createElement from './ui/createElement.js';
 import button from "./ui/button"
 import inputText from "./ui/input"
-import { linesWrapper } from "./ui/linesWrapper"
 
 const renderHTML = () => {
-
     const wrapper = createElement('div', 'wrapper');
     document.body.appendChild(wrapper);
 
     const form = createElement('form', 'form');
     wrapper.appendChild(form);
 
-    // const input = createElement('input', 'form__input');
-    // input.type = "text";
-    // input.id = 'input';
-    // input.placeholder = 'Enter numbers';
     const input = inputText('', 'form__input', 'input', 'Enter numbers');
     form.appendChild(input);
 
     const buttonsInner = createElement('div', "form__button-inner");
     form.appendChild(buttonsInner);
 
-    // const buttonBack = createElement('button', "form__button", "назад");
-    // buttonBack.id = "dec";
-    // buttonBack.type = "button";
-    // buttonsInner.appendChild(buttonBack);
-    //
-    // const buttonNext = createElement('button', "form__button", "вперед");
-    // buttonNext.id = "inc";
-    // buttonNext.type = "button";
-    // buttonsInner.appendChild(buttonNext);
-
-    let deleteValue = button('form__button', 'удалить значение', 'del');
+    const deleteValue = button('form__button', 'очистить поле ввода', 'del');
     buttonsInner.appendChild(deleteValue);
 
-    const containerForColumns = createElement('div', 'columns__container');
-    containerForColumns.appendChild(linesWrapper);
-    document.body.appendChild(containerForColumns);
+    const linesWrapper = createElement('div', 'lines__wrapper');
+    document.body.appendChild(linesWrapper);
 
-    // const linesWrapper = createElement('div', 'lines__wrapper');
-    // document.body.appendChild(linesWrapper);
-
-    return {
-        "input": input,
-        // "butBack": buttonBack,
-        // "butNext": buttonNext,
-        "delVal": deleteValue
-
-    };
 };
 
 
--- src/ui/buttonsInner.js
+++
@@ -1,3 +0,0 @@
-import createElement from "./createElement";
-
-export const buttonsInner = createElement('div', "form__button-inner");
