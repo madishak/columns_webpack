25fa4dc90735bb0bf927a7ff6c778407b73c8ef6
madishak <33802709+madishak@users.noreply.github.com>
2019-07-15 12:42:49 +0300

    Changed bubble sort

--- src/sort.js
+++ src/sort.js
@@ -16,21 +16,23 @@ class Sort {
             }
 
         }
+        console.log(this.arr);
         return this.arr;
     }
 
 
     increaseSort() {
 
-        for (let i = 0; i < this.arr.length-1; i++) {
-            for (let j = 0; j < this.arr.length-1-i; j++) {
-                if (this.arr[j+1] < this.arr[j]) {
-                    [this.arr[j+1], this.arr[j]] = [this.arr[j], this.arr[j+1]];
-                    this.listOfIndexes = [j, j + 1, ...this.listOfIndexes];
+        for (let i = 1; i < this.arr.length; i++) {
+            for (let j = 1; j < i+1; j++) {
+                if (this.arr[j-1] > this.arr[j]) {
+                    [this.arr[j-1], this.arr[j]] = [this.arr[j], this.arr[j-1]];
+                    this.listOfIndexes = [j-1, j, ...this.listOfIndexes]; //sort's steps(array's indexes)
                     return this.arr;
                 }
             }
         }
+        console.log(this.arr);
         return this.arr;
 
     }
