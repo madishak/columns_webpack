c82a967aab478fab49e94fb2bd50607dee1d260d
Madina Karachai <madina.karachai@ru.axxonsoft.com>
2019-07-23 16:20:31 +0300

    Changed code

--- src/sort.js
+++ src/sort.js
@@ -2,6 +2,8 @@ class Sort {
     constructor(array) {
         this.arr = array.slice(0);
         this.listOfIndexes = [];
+        //this.newArr = this.listOfIndexes.reverse();
+        //this.listOfIndexesNext = [];
     }
 
 
@@ -23,24 +25,48 @@ class Sort {
 
     increaseSort() {
 
-        for (let i = 0; i < this.arr.length-1; i++) {
-            let isSwap = false;
-            for (let j = 0; j < this.arr.length-1-i; j++) {
-                if (this.arr[j] > this.arr[j+1]) {
-                    [this.arr[j], this.arr[j+1]] = [this.arr[j+1], this.arr[j]];
-                    isSwap = true;
-                    console.log(j,j+1);
-                    this.listOfIndexes = [j, j+1, ...this.listOfIndexes]; //sort's steps(array's indexes)
-                    return this.arr;
-                }
-            }
-            if (!isSwap) {
-                break;
-            }
-        }
-        console.log(this.arr);
-        return this.arr;
 
+            for (let i = 0; i < this.arr.length; i++) {
+                for (let j = 0; j < this.arr.length-i-1; j++) {
+                    if (this.arr[j] > this.arr[j+1]) {
+                        [this.arr[j], this.arr[j+1]] = [this.arr[j+1], this.arr[j]];
+                        this.listOfIndexes = [j, j+1, ...this.listOfIndexes];
+                        //this.listOfIndexesNext = [...this.listOfIndexesNext, j, j+1];
+
+                        return  this.arr;
+                    }
+
+                }
+
+            }
+
+        return  this.arr;
+
+
+    }
+
+    change() {
+        let listOfIndexes = this.listOfIndexes.slice(0);
+        //console.log(this.arr);
+        console.log(listOfIndexes);
+
+        for (let j = 0; j < this.arr.length; j++) {
+
+
+                    if (listOfIndexes.length !== 0) {
+                        [this.arr[listOfIndexes[0]], this.arr[listOfIndexes[1]]] = [this.arr[listOfIndexes[1]], this.arr[listOfIndexes[0]]];
+                        listOfIndexes.shift(listOfIndexes[0]);
+                        listOfIndexes.shift(listOfIndexes[1]);
+
+                        console.log(listOfIndexes);
+                        return this.arr;
+
+                    }
+
+
+        }
+
+        return this.arr;
     }
 
 
