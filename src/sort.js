83ba3db96cb469b991973d883c36248884ef6f27
Madina Karachai <madina.karachai@ru.axxonsoft.com>
2019-07-24 16:02:20 +0300

    Modified sort algorithm

--- src/sort.js
+++ src/sort.js
@@ -2,7 +2,6 @@ class Sort {
     constructor(array) {
         this.arr = array.slice(0);
         this.listOfIndexes = [];
-        //this.newArr = this.listOfIndexes.reverse();
         this.listOfIndexesBack = [];
     }
 
@@ -14,65 +13,40 @@ class Sort {
                 this.listOfIndexes = [this.listOfIndexesBack[0], this.listOfIndexesBack[1], ...this.listOfIndexes];
                 this.listOfIndexesBack.shift(this.listOfIndexesBack[0]);
                 this.listOfIndexesBack.shift(this.listOfIndexesBack[1]);
-
-
                 return this.arr;
             }
 
         }
-        //console.log(this.arr);
-        console.log(this.listOfIndexes);
         return this.arr;
     }
 
 
-    increaseSort() {
+    bubbleSort() {
+        const arrayCopy = this.arr.slice(0);
 
-
-            for (let i = 0; i < this.arr.length-1; i++) {
-                for (let j = 0; j < this.arr.length-i-1; j++) {
-                    if (this.arr[j] > this.arr[j+1]) {
-                        [this.arr[j], this.arr[j+1]] = [this.arr[j+1], this.arr[j]];
-                        //this.listOfIndexes = [j, j+1, ...this.listOfIndexes];
+            for (let i = 0; i < arrayCopy.length; i++) {
+                for (let j = 0; j < arrayCopy.length-i-1; j++) {
+                    if (arrayCopy[j] > arrayCopy[j+1]) {
+                        [arrayCopy[j], arrayCopy[j+1]] = [arrayCopy[j+1], arrayCopy[j]];
                         this.listOfIndexes = [...this.listOfIndexes, j, j+1];
-
-                        //return  this.arr;
                     }
-
                 }
-
             }
-            console.log(this.listOfIndexes);
-
-        return  this.arr;
-
-
+        return  arrayCopy;
     }
 
-    change() {
-       // let listOfIndexes = this.listOfIndexes.slice(0);
-        console.log(this.arr);
-        //console.log(this.listOfIndexes);
-
+    increaseSort() {
         for (let j = 0; j < this.arr.length; j++) {
 
 
                     if (this.listOfIndexes.length !== 0) {
                         [this.arr[this.listOfIndexes[0]], this.arr[this.listOfIndexes[1]]] = [this.arr[this.listOfIndexes[1]], this.arr[this.listOfIndexes[0]]];
-                        console.log(this.arr[this.listOfIndexes[0]], this.arr[this.listOfIndexes[1]]);
-
                         this.listOfIndexesBack = [this.listOfIndexes[0], this.listOfIndexes[1], ...this.listOfIndexesBack];
                         this.listOfIndexes.shift(this.listOfIndexes[0]);
                         this.listOfIndexes.shift(this.listOfIndexes[1]);
-
-                        //console.log(this.listOfIndexesBack);
                         return this.arr;
-
                     }
-
-
         }
-
         return this.arr;
     }
 
