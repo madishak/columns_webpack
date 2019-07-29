d38f6fcd2b187bb5134154f46f57e882d2deceda
Madina Karachai <madina.karachai@ru.axxonsoft.com>
2019-07-29 16:44:32 +0300

    Created controlled input

--- src/index.js
+++ src/index.js
@@ -10,24 +10,12 @@ renderHTML();
 
 const inputShow = document.getElementById('input');
 
-const strToArray = (str) => {
-    return str.split("").map(element => {
-        let result;
-        Number(element);
-
-        if (isNaN(element)) {
-            result = '';
-        } else {
-            result = element;
-        }
-
-        return result;
-
-    });
-};
-
+const strToArray = (str) => str.split("").map(element => Number(element));
 
 const renderCollection = inputValue => {
+    if (inputValue.length === 0) {
+        return;
+    }
 
     let sort = new Sort(inputValue);
     let draw = new Draw(inputValue);
@@ -35,17 +23,17 @@ const renderCollection = inputValue => {
     draw.drawArray();
 
 
-    const errorMessage = createElement('div', 'error__message', 'Введите цифры, вместо букв');
+    //const errorMessage = createElement('div', 'error__message', 'Введите цифры, вместо букв');
 
-    inputValue.forEach(value => {
-        //console.log(value);
-        if (value === '') {
-            return draw.linesButtonsContainer.appendChild(errorMessage);
-
-        }
-
-
-    });
+    // inputValue.forEach(value => {
+    //     //console.log(value);
+    //     if (value === '') {
+    //         return draw.linesButtonsContainer.appendChild(errorMessage);
+    //
+    //     }
+    //
+    //
+    // });
 
 
     const buttonBack = button("lines__button", "назад", 'dec');
@@ -62,16 +50,11 @@ const renderCollection = inputValue => {
 
     draw.linesButtonsContainer.appendChild(buttonsInner);
 
-
-
-
-
 };
-//
-// inputShow.addEventListener('change', () => {
-//     const newArr = strToArray(inputShow.value);
-//     return renderCollection(newArr);
-// });
+
+inputShow.addEventListener('input', (evt) => {
+    inputShow.value = evt.target.value.match(/\d+/g);
+});
 
 let startRender = document.querySelector('#start');
 startRender.addEventListener('click', () => {
