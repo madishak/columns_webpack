7683dd5451b5ca4b3200b9d4fee143d38822b04b
Madina Karachai <madina.karachai@ru.axxonsoft.com>
2019-08-27 18:03:34 +0300

    Renamed file

---
+++ src/ui/columnsWrapper.js
@@ -0,0 +1,3 @@
+import createElement from './createElement.js';
+
+export const columnsWrapper = createElement({'tag':'div', 'class':'columns__wrapper'});
--- src/ui/linesWrapper.js
+++
@@ -1,3 +0,0 @@
-import createElement from './createElement.js';
-
-export const linesWrapper = createElement({'tag':'div', 'class':'lines__wrapper'});
