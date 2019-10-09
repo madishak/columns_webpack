03492652df6b5a132dcf5ccb51467e933c2c5967
Madina Karachai <medinakarachai@mail.ru>
2019-10-09 18:03:02 +0300

    Saved states, but they don't work correct

--- src/ui/bubbleSortApp.js
+++ src/ui/bubbleSortApp.js
@@ -33,113 +33,82 @@ const bubbleSortApp = () => {
   });
   buttonsInner.appendChild(startRender);
 
-  wrapper.appendChild(columnsWrapper);
-
   let currentStates = [];
-
-  // const addArray = () =>
-  //   currentStates.map(value => {
-  //     console.log(value);
-  //     //renderCollection(value);
-  //   });
-
-  // const removeArray = index => {
-  //   return currentStates.filter((element, ind) => index !== ind);
-  // };
 
   const strToArray = str => str.split("").map(element => Number(element));
   let ind = 0;
 
-  // let newState = removeArray(closeButton.id);
-
-  // const newState = removeArray(1);
-  // const render = (values) => {
-
-  class CurrentStates {
-    constructor(value) {
-      this.state = value;
+  class Render {
+    constructor() {
       this.currentStates = [];
     }
 
-    addArray() {
-      this.currentStates = [...this.currentStates, this.state];
-      return this.currentStates;
+    removeArray(index) {
+      const newState = this.currentStates.filter((elem, ind) => index !== ind);
+      // console.log(index);
+      // console.log(newState);
+      return newState;
     }
 
-    removeArray(index) {
-      //const newState =  this.state.filter((element, ind) => index !== ind);
-      this.state = 0;
-      return this.state;
+    renderCollection() {
+      console.log(this.currentStates);
+      this.currentStates.map(elem => {
+        if (elem.length === 0) {
+          return;
+        }
+
+        let sort = new Sort(elem);
+        let draw = new Draw(elem);
+
+        draw.drawArray();
+
+        const closeButton = button({
+          class: "columns__close",
+          text: "&times;",
+          id: ind++
+        });
+
+        closeButton.addEventListener("click", () => {
+          //console.log(closeButton.id);
+          this.removeArray(closeButton.id);
+        });
+
+        draw.columnsButtonsContainer.prepend(closeButton);
+
+        const buttonBack = button({
+          class: "columns__button",
+          text: "назад",
+          id: "dec"
+        });
+
+        const buttonNext = button({
+          class: "columns__button",
+          text: "вперед",
+          id: "inc"
+        });
+
+        buttonBack.addEventListener("click", () =>
+          draw.movement(sort.decreaseSort())
+        );
+        buttonNext.addEventListener("click", () =>
+          draw.movement(sort.increaseSort())
+        );
+
+        const buttonsInner = createElement({
+          tag: "div",
+          class: "columns__button-inner"
+        });
+        buttonsInner.append(buttonBack, buttonNext); //experimental technology "Node.append()"
+
+        draw.columnsButtonsContainer.appendChild(buttonsInner);
+      });
+      wrapper.appendChild(columnsWrapper);
+      this.currentStates = [];
+      return this.currentStates;
     }
   }
 
-  const renderCollection = inputValue => {
-    // inputValue.map(elem => {
-    if (inputValue.length === 0) {
-      return;
-    }
-
-    //let currentStatesLength = currentStates.length - 1;
-
-    let sort = new Sort(inputValue);
-    let draw = new Draw(inputValue);
-    // let draw = new Draw(currentStates[currentStatesLength]);
-
-    draw.drawArray();
-
-    const setId = () => {};
-    const closeButton = button({
-      class: "columns__close",
-      text: "&times;",
-      id: ind++
-    });
-
-    closeButton.addEventListener("click", () => {
-      //console.log(closeButton.id);
-      // renderCollection();
-      let currentStates = new CurrentStates(inputValue);
-
-      currentStates.removeArray(closeButton.id);
-      console.log(currentStates.currentStates);
-    });
-
-    draw.columnsButtonsContainer.prepend(closeButton);
-
-    const buttonBack = button({
-      class: "columns__button",
-      text: "назад",
-      id: "dec"
-    });
-
-    const buttonNext = button({
-      class: "columns__button",
-      text: "вперед",
-      id: "inc"
-    });
-
-    buttonBack.addEventListener("click", () =>
-      draw.movement(sort.decreaseSort())
-    );
-    buttonNext.addEventListener("click", () =>
-      draw.movement(sort.increaseSort())
-    );
-
-    const buttonsInner = createElement({
-      tag: "div",
-      class: "columns__button-inner"
-    });
-    buttonsInner.append(buttonBack, buttonNext); //experimental technology "Node.append()"
-
-    draw.columnsButtonsContainer.appendChild(buttonsInner);
-    // });
-    // return inputValues;
-  };
-
-  //   return values.map(arr => {
-  //     renderCollection(arr);
-  //   });
-  //
-  // };
+  const render = new Render();
 
   input.addEventListener("input", evt => {
     input.value = evt.target.value.match(/\d+/g);
@@ -148,13 +117,9 @@ const bubbleSortApp = () => {
   startRender.addEventListener("click", () => {
     const newArr = strToArray(input.value);
 
-    //console.log(currentStates);
-    // addArray();
-    let currentState = new CurrentStates(newArr);
-    // console.log(currentState.addArray(newArr));
-    currentStates = [...currentStates, currentState.state];
-    // console.log(currentStates);
-    renderCollection(currentState.state);
+    render.currentStates = [...render.currentStates, newArr];
+
+    render.renderCollection();
   });
 
   return wrapper;
