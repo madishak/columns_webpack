d39a903b51ef9fedf77776924f30dfa2f032ca93
Madina Karachai <C:\Users\\AppData\Roaming\The Bat!>
2019-11-06 00:36:37 +0300

    Tried to do sort memorialization

--- src/ui/sort.ts
+++ src/ui/sort.ts
@@ -3,55 +3,73 @@ class Sort {
   arrCopy: number[];
   listOfIndexes: number[];
   currentIndex: number;
+  currentPosition: number;
   public constructor(array: number[]) {
     this.arr = array;
     this.arrCopy = [...this.arr];
     this.listOfIndexes = [];
     this.currentIndex = 0;
+    this.currentPosition = 0;
   }
 
   public increaseSort(): number[] {
-    for (let i = 0; i < this.arrCopy.length - 1; i += 1) {
-      for (let j = this.currentIndex; j < this.arrCopy.length - i - 1; j += 1) {
-        if (this.arrCopy[j] > this.arrCopy[j + 1]) {
-          Sort.swapElements(this.arrCopy, j);
-          this.listOfIndexes = [...this.listOfIndexes, j];
-          this.currentIndex = j;
-          return this.arrCopy;
+    if (this.listOfIndexes.length === this.currentIndex) {
+      for (let i = 0; i < this.arrCopy.length - 1; i += 1) {
+        for (let j = this.currentPosition; j < this.arrCopy.length - i - 1; j += 1) {
+          if (this.arrCopy[j] > this.arrCopy[j + 1]) {
+            Sort.swapElements(this.arrCopy, j);
+            this.listOfIndexes = [...this.listOfIndexes, j];
+            this.currentPosition = j;
+            this.currentIndex = this.listOfIndexes.length;
+            return this.arrCopy;
+          }
         }
+        this.currentPosition = 0;
       }
-      this.currentIndex = 0;
     }
+    // else {
+    //   console.log(this.currentIndex);
+    //  Sort.swapElements(this.arrCopy, this.currentIndex);
+    //   this.currentPosition = this.listOfIndexes[this.currentIndex-1];
+    // }
+
     return this.arrCopy;
   }
 
   public decreaseSort(): number[] {
+    console.log(this.listOfIndexes);
+    let ReversedListOfIndexes = this.listOfIndexes.reduceRight((acc, elem) => [...acc, elem], []);
+    console.log(ReversedListOfIndexes);
+
     // if (this.currentIndex > this.listOfIndexes.length) {
     //   this.currentIndex = 0;
     //   return this.currentIndex;
     // }
-    // for (let i = this.currentIndex; i <= this.listOfIndexes.length; i++) {
-    //   for (let j = 0; j < this.arrCopy.length; j++) {
-    //
-    //     if (this.listOfIndexes[i] === j) {
-    //       Sort.swapElements(this.arrCopy, j);
-    //       this.currentIndex = this.currentIndex + 1;
-    //      // console.log(this.listOfIndexes[i], j, this.currentIndex);
-    //       return this.arrCopy;
-    //     }
+    console.log(this.currentPosition);
+    for (let i =  0; i < ReversedListOfIndexes.length; i++) {
+      // console.log(ReversedListOfIndexes[i]);
+      for (let j = ReversedListOfIndexes[i]; j < this.arrCopy.length; j++) {
+
+        if (ReversedListOfIndexes[i] === j) {
+          Sort.swapElements(this.arrCopy, j);
+          this.currentPosition = j;
+         // console.log(this.listOfIndexes[i], j, this.currentIndex);
+          return this.arrCopy;
+        }
+      }
+      ///this.currentIndex =  this.currentIndex++;
+    }
+
+    return this.arrCopy;
+    // for (let i = 0; i < this.arrCopy.length; i += 1) {
+    //   if (this.listOfIndexes.length !== 0) {
+    //     Sort.swapElements(this.arrCopy, this.listOfIndexes[this.listOfIndexes.length - 1]);
+    //     this.listOfIndexes.pop();
+    //     this.currentIndex = this.listOfIndexes[this.listOfIndexes.length - 1];
+    //     return this.arrCopy;
     //   }
     // }
-    // this.currentIndex = 0;
     // return this.arrCopy;
-    for (let i = 0; i < this.arrCopy.length; i += 1) {
-      if (this.listOfIndexes.length !== 0) {
-        Sort.swapElements(this.arrCopy, this.listOfIndexes[this.listOfIndexes.length - 1]);
-        this.listOfIndexes.pop();
-        this.currentIndex = this.listOfIndexes[this.listOfIndexes.length - 1];
-        return this.arrCopy;
-      }
-    }
-    return this.arrCopy;
   }
 
   static swapElements(array: number[], index: number): void {
