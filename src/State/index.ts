1cdb7d458c921e9407b3908dbd9ab3b553ff6152
Madina Karachai <medinakarachai@mail.ru>
2020-03-10 18:07:30 +0300

    Deleted unnecessary code

--- src/State/index.ts
+++ src/State/index.ts
@@ -12,26 +12,6 @@ class State {
     this.state.sorters = value.sorters;
     return this.state;
   }
-
-  // public removeSorter(index: number): StateTypes {
-  //   let stateItems: StateTypes[] = [];
-  //   for (const i in this.state) {
-  //     stateItems = [...stateItems, i];
-  //   }
-  //   // stateItems = [...stateItems, this.state];
-  //   stateItems = stateItems.filter((elem: SorterType) => elem.sorterId !== index);
-  //   return this.state;
-  // }
-  //
-  // public updateSorter(index: number, newState: number[]): void {
-  //   return this.state.sorters.forEach((elem: SorterType): number[] => {
-  //     if (index === elem.sorterId) {
-  //       elem.sorterArr = newState;
-  //       return elem.sorterArr;
-  //     }
-  //     return elem.sorterArr;
-  //   });
-  // }
 }
 
 export default State;
