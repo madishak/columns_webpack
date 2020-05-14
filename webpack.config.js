f9c4a445a95927d69c0d39ffc4f0c410ba495e34
Madina Karachai <medinakarachai@mail.ru>
2020-05-14 18:46:09 +0300

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,32 +5,32 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.ts'
+    app: './src/index1.ts',
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Madina Karachai - Columns',
       favicon: 'columns.ico',
-      template: path.resolve(__dirname, 'src/index.html')
-    })
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.m?js$/,
@@ -38,15 +38,15 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.tsx?$/,
         use: ['ts-loader'],
-        exclude: /node_modules/
-      }
-    ]
-  }
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
