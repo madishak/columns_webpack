d5d230ef147d76745bd8b0767abd6c9c7ea3a6d6
Madina Karachai <medinakarachai@mail.ru>
2020-05-15 11:25:57 +0300

    Upadated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index1.ts',
+    app: './src/index.ts',
   },
   devtool: 'inline-source-map',
   devServer: {
